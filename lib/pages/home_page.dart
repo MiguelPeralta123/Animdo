d5e4b5e6c8c4f06fc31863f0e03af0165971c324
Miguel Peralta <maikperalta248@gmail.com>
2024-03-27 11:08:53 -0700

    Background and rotating star animations

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -10,8 +12,22 @@ class HomePage extends StatefulWidget {
   }
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+
   double _buttonDiameter = 100;
+  final Tween<double> _backgroundScale = Tween<double>(begin: 0.0, end: 1.0);
+  late AnimationController _starIconAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    // The "with SingleTickerProviderStateMixin" allows to initialize the animation controller
+    _starIconAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    );
+    _starIconAnimationController.repeat();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +36,34 @@ class _HomePageState extends State<HomePage> {
           clipBehavior: Clip.none,
           children: [
             _pageBackground(),
-            _circularAnimatedButton(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _circularAnimatedButton(),
+                _starAnimatedIcon(),
+              ],
+            ),
           ],
         ),
     );
   }
 
   Widget _pageBackground() {
-    return Container(
-      color: Colors.orange,
+    return TweenAnimationBuilder(
+      tween: _backgroundScale,
+      curve: Curves.easeInOutCubicEmphasized,
+      duration: const Duration(seconds: 1),
+      builder: (context, scale, child) {
+        return Transform.scale(
+          scale: scale,
+          child: child,
+        );
+      },
+      child: Container(
+        color: Colors.orange,
+      ),
     );
   }
 
@@ -59,6 +94,23 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _starAnimatedIcon() {
+    return AnimatedBuilder(
+      animation: _starIconAnimationController.view,
+      builder: (context, child) {
+        return Transform.rotate(
+          angle: _starIconAnimationController.value * 2 * pi,
+          child: child,
+        );
+      },
+      child: const Icon(
+        Icons.star,
+        size: 100,
+        color: Colors.white,
       ),
     );
   }
